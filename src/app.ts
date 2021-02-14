48f73f42e5ea8c3e5fd20e061ba81429a7e840a7
Louis Quinnell <_@ziorobo.co>
2021-02-14 18:41:57 +1100

    Set up probot app

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import { Probot } from "probot"
+
+export default (app: Probot) => {
+  app.log("App loaded!")
+
+  app.on("issues.opened", async context => {
+    return context.octokit.issues.createComment(
+      context.issue({ body: "Ding!" })
+    )
+  })
+}
