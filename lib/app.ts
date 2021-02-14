828e2197dc4585f666c50411f620ae5b080542f1
Louis Quinnell <_@ziorobo.co>
2021-02-14 22:20:07 +1100

    Log probot version

--- lib/app.ts
+++ lib/app.ts
@@ -4,6 +4,7 @@ import { Probot } from "probot"
 
 export default (app: Probot) => {
   app.log("App loaded!")
+  app.log("version: ", app.version)
 
   app.on("issues.opened", async context => {
     return context.octokit.issues.createComment(
